73b63b034513e159af1b1b6b9ab300fbdc53e1a8
Rob Walch <rwalch@gmail.com>
2012-11-09 23:35:14 -0500

    ec namespace, refactored main

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,79 @@
-var camera, scene, renderer;
-var geometry, material, mesh;
+var ec = ec || {};
 
-init();
-animate();
+ec.width = 960;//1280;
+ec.height = 540;//720;
 
-function init() {
+(function() {
+	'use strict';
+	var THREE = window.THREE;
+	var requestAnimationFrame = window.requestAnimationFrame;
+	var camera, scene, renderer;
+	var geometry, material, mesh;
+	var delta, deltaTime;
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
-    camera.position.z = 1000;
+	var core = {
 
-    scene = new THREE.Scene();
 
-    geometry = new THREE.CubeGeometry( 200, 200, 200 );
-    material = new THREE.MeshBasicMaterial( { color: 0xff0000, wireframe: true } );
+		init: function(time) {
+			camera = new THREE.PerspectiveCamera( 75, ec.width / ec.height, 1, 10000 );
+		    //camera = new THREE.OrthographicCamera( ec.width / - 2, ec.width / 2, ec.height / 2, ec.height / - 2, 1, 10000 );
+		    //camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 1, 10000 );
+		    camera.position.z = 100;
+		    
+		    scene = new THREE.Scene();
 
-    mesh = new THREE.Mesh( geometry, material );
-    scene.add( mesh );
+		    geometry = new THREE.CubeGeometry( 200, 200, 200 );
+		    material = new THREE.MeshBasicMaterial( {
+				color: 0xff0000, wireframe: true
+		    } );
 
-    renderer = new THREE.CanvasRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+		    mesh = new THREE.Mesh( geometry, material );
+		    scene.add( mesh );
 
-    //$("hero-unit").append( renderer.domElement );
-    document.body.appendChild( renderer.domElement );
+		    renderer = new THREE.CanvasRenderer();
+		    renderer.setSize( ec.width, ec.height );
 
-}
+		    //$("hero-unit").append( renderer.domElement );
+		    renderer.domElement.style.position = 'absolute';
+			renderer.domElement.style.left = '0px';
+			renderer.domElement.style.top = '0px';
+		    document.body.appendChild( renderer.domElement );
 
-function animate() {
 
-    // note: three.js includes requestAnimationFrame shim
-    requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
 
-    renderer.render( scene, camera );
+		    deltaTime = time;
+		    requestAnimationFrame( core.animate );
+		},
 
-}
+		animate: function(time) {
+
+			delta = time - deltaTime;
+			deltaTime = time;
+
+			// note: three.js includes requestAnimationFrame shim
+		    requestAnimationFrame( core.animate );
+
+		    mesh.rotation.x += 0.01;
+		    mesh.rotation.y += 0.02;
+
+		    camera.position.z++;
+		    //camera.lookAt(mesh.position);
+		    // camera.left--;
+		    // camera.top--;
+		    // camera.right++;
+		    // camera.bottom++;
+		    //camera.updateProjectionMatrix();
+		    //console.log(camera.position.z, camera.left);
+
+		    renderer.render( scene, camera );
+
+		}
+
+	};
+
+	requestAnimationFrame( core.init );
+
+	ec.Core = core;
+
+})();
