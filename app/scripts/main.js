58e014c66410526ff2ee7662993a2b6f10279d9c
Rob Walch <rwalch@gmail.com>
2012-11-30 02:17:47 -0500

    removed utils.js because of name conflict

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -305,6 +305,34 @@ var ec = ec || {'version': '0.1.108'};
 		}
 	};
 
+	// utils
+
+	ec.extend = function( target, source ) {
+        for ( var prop in source ) {
+			if ( source.hasOwnProperty( prop ) ) {
+	            if ( !target.hasOwnProperty( prop ) ) {
+					var copy = source[ prop ];
+					if (copy !== undefined) {
+						target[ prop ] = source[ prop ];
+					}
+	            }
+	        }
+        }
+        return target;
+    };
+
+    ec.create = function(obj) {
+		function F() {}
+		F.prototype = obj;
+		return new F();
+    };
+
+	ec.delegate = function(obj, func) {
+		return function() {
+			return func.apply(obj, arguments);
+		};
+	};
+
 	// polyfills
 
 	if (!Date.now) {
