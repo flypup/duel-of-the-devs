701d08cae210eeacc21b9ae5707b85296fd1c695
Rob Walch <rwalch@gmail.com>
2013-02-23 19:39:01 -0500

    sealed credits view object members

--- app/scripts/views/Canvas2dCreditsView.js
+++ app/scripts/views/Canvas2dCreditsView.js
@@ -4,10 +4,13 @@
 	var ec = window.ec;
 
 	var Canvas2dCreditsView = ec.Canvas2dCreditsView = function() {
-		this.creditsTime = -1;
-		this.skipAfter = 3500;
-		
-		this.overlay = ec.getImage('img/ui/credits.png');
+		ec.extend(this, {
+			width: 0,
+			height: 0,
+			creditsTime: -1,
+			skipAfter: 3500,
+			overlay: ec.getImage('img/ui/credits.png')
+		});
 
 		if (ec.debug > 1) {
 			Object.seal(this);
@@ -15,38 +18,38 @@
 	};
 
 	// TODO: Extend 2d Overlay/Layer View
-	var proto = Canvas2dCreditsView.prototype;
+	Canvas2dCreditsView.prototype = {
+		init: function(context) {
+			this.creditsTime = 0;
 
-	proto.init = function(context) {
-		this.creditsTime = 0;
+			context.setTransform(1, 0, 0, 1, 0, 0);
+			context.fillStyle = '#000';
+			context.globalAlpha = 0.1;
+		},
 
-		context.setTransform(1, 0, 0, 1, 0, 0);
-		context.fillStyle = '#000';
-		context.globalAlpha = 0.1;
-	};
+		draw: function(context, delta) {
+			context.fillRect(0, 0, this.width, this.height);
+			context.globalAlpha = Math.min(1, context.globalAlpha + 0.1);
 
-	proto.draw = function(context, delta) {
-		context.fillRect(0, 0, this.width, this.height);
-		context.globalAlpha = Math.min(1, context.globalAlpha + 0.1);
+			this.creditsTime += delta;
+			var scrollPx = Math.floor(this.creditsTime * 24 / 1000) * 4;
 
-		this.creditsTime += delta;
-		var scrollPx = Math.floor(this.creditsTime * 24 / 1000) * 4;
+			var overlay = this.overlay;
+			if (overlay.height) {
+				var scale = this.height / overlay.height;
 
-		var overlay = this.overlay;
-		if (overlay.height) {
-			var scale = this.height / overlay.height;
+				var x = this.width - overlay.width * scale;
+				//var y = this.height - overlay.height * scale;
+				var y = Math.max(-258 * scale, this.height - scrollPx);
 
-			var x = this.width - overlay.width * scale;
-			//var y = this.height - overlay.height * scale;
-			var y = Math.max(-258 * scale, this.height - scrollPx);
+				context.drawImage(overlay, x/2, y, overlay.width * scale, overlay.height * scale);
+			}
+		},
 
-			context.drawImage(overlay, x/2, y, overlay.width * scale, overlay.height * scale);
+		resize: function(width, height, ratio) {
+			this.width  = width  * ratio;
+			this.height = height * ratio;
 		}
 	};
-
-	proto.resize = function(width, height, ratio) {
-		this.width  = width  * ratio;
-		this.height = height * ratio;
-	};
-
+	
 })(window);
